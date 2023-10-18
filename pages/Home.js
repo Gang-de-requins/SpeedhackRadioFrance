c047c56aff25e8f4e2a6781bc605d97c44dee44e
Ethan SAUX <eowin.saux@gmail.com>
2023-10-18 12:10:10 +0200

    feat(app): creation of the app with tab navigator

---
+++ pages/Home.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function Home({ navigation }) {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
